6c815a865bfedd4d7062152e2ab58ea919c58e87
Christian Grewell <christian@nyu.edu>
2018-08-01 14:17:36 +0800

    First commit

---
+++ src/coffeetawk.js
@@ -0,0 +1,18 @@
+console.log('programming is fun!');
+console.log('comments are important. They allow others and yourself to better understand your code. They are also important for organizing your tasks!')
+/*
+
+*/
+
+//primatives
+//
+
+//
+let customerName = 'Christian';
+let customerBeverage = 'Americano';
+let beveragePrice = 24;
+let beverageCount = 3;
+
+console.log(customerBeverage);
+console.log(beverageCount * beveragePrice);
+console.log(customerName + ' has ordered ' + beverageCount + ' ' + customerBeverage +', for a total price of ' + beverageCount * beveragePrice + ' RMB.');
